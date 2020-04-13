4bcdf90d78ef148c080408c9258db09c1f1ed745
RafaelEmery <rafael.emerycade@gmail.com>
2020-04-13 19:16:44 -0300

    Mudanças em potão

--- resources/views/sistema/comentarios/index.blade.php
+++ resources/views/sistema/comentarios/index.blade.php
@@ -20,7 +20,7 @@
         <!-- Collapsable Card Example -->
         <div class="card shadow mb-4">
             <!-- Card Header - Accordion -->
-            <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+            <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseCardExample">
                 <h6 class="m-0 font-weight-bold text-primary"> {{ $post->titulo }} </h6>
             </a>
             <!-- Card Content - Collapse -->
--- resources/views/sistema/posts/index.blade.php
+++ resources/views/sistema/posts/index.blade.php
@@ -5,7 +5,7 @@
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
   <h1 class="h3 mb-0 text-gray-800">Posts</h1>
-  <a  href="{{route('sistema.posts.create')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary"><i class="fas fa-plus fa-sm"></i>   Novo Post</a>
+  <a  href="{{route('sistema.posts.create')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary"><i class="fas fa-plus fa-sm"></i> &nbsp; Novo Post</a>
 </div>
 
 @endsection
