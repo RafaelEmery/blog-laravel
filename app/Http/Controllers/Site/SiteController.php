39a0649c931e012f01c60641e883caf0331a6594
RafaelEmery <rafael.emerycade@gmail.com>
2020-05-07 01:18:45 -0300

    Valores sendo exibidos na view index

--- app/Http/Controllers/Site/SiteController.php
+++ app/Http/Controllers/Site/SiteController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Site;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class SiteController extends Controller
 {
     public function index()
     {
-        return view('site.index');
+        $posts = Post::where('destaque', false)->latest()->get();
+        $postDestacado = Post::where('destaque', true)->first();
+
+        return view('site.index', compact('posts', 'postDestacado'));
     }
 
     public function post()
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -5,7 +5,7 @@
 @section('conteudo')
 
 <!-- Page Header -->
-<header class="masthead" style="background-image: {{asset('templateSite/img/home-bg.jpg')}}">
+<header class="masthead" style="background-image: {{asset('templateSite/img/home-bg.jpg')}};">
     <div class="overlay"></div>
     <div class="container">
         <div class="row">
@@ -23,59 +23,41 @@
 <div class="container">
     <div class="row">
     <div class="col-lg-8 col-md-10 mx-auto">
+    
+    <!-- Post destacado -->
     <div class="post-preview">
         <a href="post.html">
         <h2 class="post-title">
-            Man must explore, and this is exploration at its greatest
+            {{ $postDestacado->titulo }}
         </h2>
         <h3 class="post-subtitle">
-            Problems look mighty small from 150 miles up
+            {{ $postDestacado->subtitulo }}
         </h3>
         </a>
         <p class="post-meta">Posted by
-        <a href="#">Start Bootstrap</a>
-        on September 24, 2019</p>
+        <a href="#"> {{ $postDestacado->autor }} </a>
+        on  {{ $postDestacado->created_at }} </p>
     </div>
     <hr>
+
+    <!-- Posts listados -->
+    @foreach($posts as $post)
     <div class="post-preview">
         <a href="post.html">
         <h2 class="post-title">
-            I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-        </h2>
-        </a>
-        <p class="post-meta">Posted by
-        <a href="#">Start Bootstrap</a>
-        on September 18, 2019</p>
-    </div>
-    <hr>
-    <div class="post-preview">
-        <a href="post.html">
-        <h2 class="post-title">
-            Science has not yet mastered prophecy
+            {{ $post->titulo }}
         </h2>
         <h3 class="post-subtitle">
-            We predict too much for the next year and yet far too little for the next ten.
+            {{ $post->subtitulo }}
         </h3>
         </a>
         <p class="post-meta">Posted by
-        <a href="#">Start Bootstrap</a>
-        on August 24, 2019</p>
+        <a href="#">{{ $post->autor }}</a>
+        on {{ $post->created_at }} </p>
     </div>
     <hr>
-    <div class="post-preview">
-        <a href="post.html">
-        <h2 class="post-title">
-            Failure is not an option
-        </h2>
-        <h3 class="post-subtitle">
-            Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-        </h3>
-        </a>
-        <p class="post-meta">Posted by
-        <a href="#">Start Bootstrap</a>
-        on July 8, 2019</p>
-    </div>
-    <hr>
+    @endforeach
+
     <!-- Pager -->
     <div class="clearfix">
         <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
